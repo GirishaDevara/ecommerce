f323d7a7c889e63b74cefb130f5d08941f0ec659
Girisha Devara <girisha.devara@gmail.com>
2020-06-11 23:23:08 +0530

    model.py product

--- products/models.py
+++ products/models.py
@@ -6,3 +6,14 @@ class Category(models.Model):
     
     def __str__(self):
         return self.category_name
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=300)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    Price = models.FloatField()
+    details = models.TextField()
+    Description = models.TextField()
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.product_name

a05de33c8ccfa659afbf4bf65102a10f032e5970
Girisha Devara <girisha.devara@gmail.com>
2020-06-16 13:45:11 +0530

    customer model

--- customer/models.py
+++ customer/models.py
@@ -3,12 +3,13 @@ from django.contrib.auth.models import User
 
 
 class CustomerDefaultAddress(models.Model):
-    Country_choices = ['india', 'India']
+    Country_choices = (('india', 'India'),
+                       ('USA','USA'))
     user = models.OneToOneField(User, related_name='UserProfiles', on_delete=models.CASCADE)
     name = models.CharField("Full name", max_length=1024)
     address1 = models.CharField("Address line 1", max_length=1024)
     address2 = models.CharField("Address line 2", max_length=1024)
-    zip_code = models.CharField("ZIP / Postal code", max_length=12 )
-    city = models.CharField("City", max_length=1024 )
-    country = models.CharField("Country", max_length=3, choices=Country_choices )
+    zip_code = models.CharField("ZIP / Postal code", max_length=12)
+    city = models.CharField("City", max_length=1024)
+    country = models.CharField("Country", max_length=20, choices=Country_choices)
     default = 0
